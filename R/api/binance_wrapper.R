425461ca80131256f066de87acb9b8c530320433
carychen <cary.qc@gmail.com>
2018-05-08 17:35:15 -0400

    POST order

--- R/api/binance_wrapper.R
+++ R/api/binance_wrapper.R
@@ -2,10 +2,36 @@ library(httr)
 library(RCurl)   # for base64Decode(), base64Encode(), getURLContent(), and getCurlHandle()
 library(digest)  # for hmac()
 library(RJSONIO) # for toJSON() and fromJSON()
+library(yaml)
+
+config_params <- yaml.load_file(input = "./config.yaml")[["Binance2"]]
+recvWindow <- 5000
+
+api.key <- config_params$key
+secret <- config_params$secret
+
 
 # binance api wrapper 
 base_url <- "https://api.binance.com/api/"
 api_version <- "v1/"
+
+
+
+# Check server time
+# GET /api/v1/time
+# exchange info
+get_system_time <- function(){
+  api_version <- "v1/"
+  end_point <- "time"
+  
+  url <- paste0(base_url, api_version, end_point) 
+  req <- httr::GET(url)
+  content <- httr::content(req, "text")
+  json <- jsonlite::fromJSON(content, simplifyVector = FALSE)
+  json$serverTime
+  
+}
+
 
 # V1 which does not require the users' api key; Public API Endpoints 
 
@@ -77,86 +103,7 @@ get_symbol_price_ticker <- function(symbol = NULL){
 ## New order (TRADE)
 # POST /api/v3/order  (HMAC SHA256)
 
-# https://github.com/binance-exchange/binance-official-api-docs/blob/master/rest-api.md#new-order--trade
-dfToJson <- function(df,token,uri){
-  ### DF is of the structure
-  ### Column 1: Offender GUID
-  ### Column 2: Table to update
-  ### Column 3: Name of column
-  ### Column 4: Value
-  ### Column 5: Dbind (TRUE or FALSE)
-  ### Column 6: Dbind prefix
-  
-  ## Get offender GUID from 
-  
-  #offender_guid <-retrieve_offender_id_info(token,uri,as.character(df[1,1]))
-  
-  dataToUpdate <- list()
-  dataToUpdate[["offender_guid"]] <- as.character(df[1,1])
-  dataToUpdate[["table"]] <- as.character(df[1,2])
-  
-  for(i in 1:nrow(df)){
-    
-    if(df[i,5] == TRUE){
-      
-      dataToUpdate[["body_list"]] <- c(dataToUpdate[["body_list"]],o360_pushobject_dbind(as.character(df[i,3]),as.character(df[i,6]),as.character(df[i,4])))
-      
-    } else{
-      
-      dataToUpdate[["body_list"]] <- c(dataToUpdate[["body_list"]],o360_pushobject_basic(as.character(df[i,3]),as.character(df[i,4])))
-      
-    }
-    
-  }
-  
-  dataToUpdate[["body_json"]] <- rjson::toJSON(dataToUpdate[["body_list"]])
-  
-  return(dataToUpdate)
-  
-}
 
-o360_post <- function(accessToken,
-                      organizationURI,
-                      entityName,
-                      post_content){
-  
-  header <- o360_pushheaders(accessToken)
-  
-  base_config <- "api/data/v8.1/%s"
-  config <- sprintf(base_config,entityName)
-  
-  
-  queryUrl <- modify_url(organizationURI,path=config)
-  
-  resp <- httr::POST(url = queryUrl,
-                     header,  
-                     body = post_content,
-                     encode="json"
-  )
-  
-  return(resp)
-}
-
-
-
-
-
-api.key <- "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-secret <- "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-
-order <- list(
-  symbol = "BTCUSDT",
-  side   = "BUY",
-  type   = "MARKET",
-  quantity  = "0.1",
-  timestamp = as.numeric(Sys.time()) * 1000
-)
-
-key <- base64Decode(secret, mode="raw")
-body <- toJSON(order, collapse="")
-what <- paste0(timestamp, toupper(method), req.url, body)
-
-sign <- base64Encode(hmac(key, what, algo="sha256", raw=TRUE))
 
 
 
@@ -165,15 +112,41 @@ sign <- base64Encode(hmac(key, what, algo="sha256", raw=TRUE))
 # POST /api/v3/order/test (HMAC SHA256)
 
 test_post_order <- function(){
-  api_version <- "v3/"
-  end_point   <- "order/test"
-
-  url <- paste0(base_url, api_version, end_point) 
-  req <- httr::POST(url, body = post_body,
-                    encode = "form",
-                    verbose())
   
-  content <- httr::content(req, "text")
+  # ==============================
+  url="https://api.binance.com/api/v3/order/test"
+  # url="https://api.binance.com/api/v3/order"
+  apiKey <- config_params$key
+  secretKey <- config_params$secret
+  
+  timestamp <- round(as.numeric(unclass(Sys.time()))*1000, 0)
+  
+  symbol = "LTCUSDT"
+  side   = "BUY"
+  type   = "MARKET"
+  quantity  = "0.00001"
+  
+  postmsg <- paste0("timestamp=", timestamp, "&recvWindow=", recvWindow,
+                    "&symbol=", symbol, "&side=",side, 
+                    "&type=", type, "&quantity=", quantity)
+  signature <- openssl::sha256(postmsg, key=secretKey)
+  
+  req <- (POST(url, 
+               config = add_headers('X-MBX-APIKEY' = binance_key()),
+               body=list(timestamp=timestamp,
+                         recvWindow=recvWindow, 
+                         signature=signature,
+                         symbol = symbol, 
+                         side   = side,
+                         type   = type,
+                         quantity  = quantity
+                         
+               ),
+               encode = "form"))
+  req
+  
+  content(req)
+  
   json <- jsonlite::fromJSON(content, simplifyVector = FALSE)
   json
   
@@ -183,6 +156,22 @@ test_post_order <- function(){
 
 
 
+url <- 'https://api.binance.com/api/v3/account'
+apiKey <- config_params$key
+secretKey <- config_params$secret
 
+timestamp <- round(as.numeric(unclass(Sys.time()))*1000, 0)
+# timestamp <- 1516941586
 
+postmsg <- paste0("timestamp=", timestamp, "&recvWindow=", recvWindow)
+signature <- openssl::sha256(postmsg, key=secretKey)
+
+req <- (GET(url, 
+            config = add_headers('X-MBX-APIKEY' = binance_key()),
+            query=list(timestamp=timestamp,
+                recvWindow=recvWindow, 
+               signature=signature)))
+req
+
+# ======================
 
---
+++ util.R
@@ -0,0 +1,52 @@
+#' Return current UNIX timestamp in millisecond
+#' @return milliseconds since Jan 1, 1970
+#' @keywords internal
+timestamp_f <- function() {
+  as.character(round(as.numeric(Sys.time()) * 1e3))
+}
+
+
+#' Request the Binance API
+#' @param base URL
+#' @param path string
+#' @param method HTTP request method
+#' @param params list
+#' @param config httr::config
+#' @param retry allow retrying the query on failure
+#' @param retries internal counter of previous retries
+#' @return R object
+#' @keywords internal
+#' @importFrom httr GET content config add_headers
+#' @importFrom futile.logger flog.error
+query <- function(base, path, method = 'GET',
+                  params = list(), config = config(),
+                  retry = method == 'GET', retries = 0) {
+  
+  if(method == 'GET'){
+    
+    res <- tryCatch(
+      content(GET(base, config = config, path = path, query = params)),
+      error = function(e) e)
+    
+    if (inherits(res, 'error')) {
+      if (isTRUE(retry) & retries < 4) {
+        mc <- match.call()
+        mc$retries <- mc$retries + 1
+        flog.error('Query to %s/%s failed for the %sst/nd/rd/th time, retrying',
+                   base, path, mc$retries)
+        eval(mc, envir = parent.frame())
+      }
+    }  
+  }
+  
+  if(method == 'POST'){
+    browser()
+    res <- tryCatch(
+      content(POST(config = config, url = paste0(base, path), body = params, verbose())),
+      error = function(e) e)
+  }
+  
+  
+  res
+  
+}
