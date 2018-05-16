ae07bf466ed0223762f52debf1489b2572b73a21
carychen <cary.qc@gmail.com>
2018-05-16 00:46:26 -0400

    Update on the calcualtion

--- R/19_arbi_between_coins.R
+++ R/19_arbi_between_coins.R
@@ -36,49 +36,19 @@ symbol_can_be_traded_in_usd <- symbol_pair_df %>%
   transmute(baseAsset  = as.character(V1), 
          quoteAsset = as.character(V2)) 
 
-symbol_can_be_traded_in_usd <- symbol_can_be_traded_in_usd %>% 
-  mutate(temp_v     = baseAsset,
-         baseAsset  = quoteAsset, 
-         quoteAsset = temp_v) %>%
-  select(-temp_v) %>% 
-  bind_rows(symbol_can_be_traded_in_usd) 
 
 
 # ==========================================================================
 
-# Get all order pair's price 
-get_best_order_all_pair_and_reverse <- function(){
-  # Get all pairs best price (market price) and quantity from market
-  best_order_all_pairs <- get_best_order_all_pair() %>% 
-    left_join(symbol_pair_df, by = "symbol_pair") %>% 
-    mutate(original_symbol_pair = symbol_pair)
-  
-  best_order_all_pairs_reverse <- best_order_all_pairs %>% 
-    mutate(temp_v      = baseAsset, 
-           baseAsset   = quoteAsset,
-           quoteAsset  = temp_v,
-           symbol_pair = paste0(baseAsset, quoteAsset),
-           
-           temp_v      = bidPrice,
-           bidPrice    = 1/askPrice,
-           askPrice    = 1/temp_v,
-           
-           temp_v      = bidQty,
-           bidQty      = askQty,
-           askQty      = temp_v) %>% 
-    select(colnames(best_order_all_pairs))
-  
-  best_order_all_pairs <- best_order_all_pairs %>% 
-    bind_rows(best_order_all_pairs_reverse)  
-}
-
-
-# For deciding whether we trade or not
+# For deciding whether trade or not
 decide_trade_or_no <- function(O_value){
   
   # get all pairs best order book
-  order_best_all <- get_best_order_all_pair_and_reverse() %>% 
-    select(symbol_pair, original_symbol_pair, 
+  order_best_all <- get_best_order_all_pair() %>% 
+      left_join(symbol_pair_df, by = "symbol_pair") %>% 
+    select(symbol_pair,
+           baseAsset, 
+           quoteAsset,
            askPrice, askQty,
            bidPrice, bidQty,
            minQty_filter)
@@ -89,59 +59,53 @@ decide_trade_or_no <- function(O_value){
   # B: Sell B to buy O; 
   decide_trade_df <- symbol_can_be_traded_in_usd %>% 
     mutate(coin_O  = "USDT",
-           pair_OA = paste0(coin_O, baseAsset),
-           pair_AB = paste0(baseAsset, quoteAsset),
+           pair_OA = paste0(baseAsset, coin_O),
+           pair_AB = paste0(quoteAsset, baseAsset),
            pair_BO = paste0(quoteAsset, coin_O)) %>% 
+    left_join(order_best_all %>% select(symbol_pair,
+                                        AB_pair_askPrice = askPrice,
+                                        AB_pair_askQty   = askQty,
+                                        AB_minQty_filter = minQty_filter),
+              by = c("pair_AB" = "symbol_pair")) %>% 
+    filter(!is.na(AB_pair_askPrice)) %>% 
     
-    left_join(order_best_all %>% 
-                select(symbol_pair,
-                       OA_pair_askPrice = askPrice,
-                       OA_pair_askQty   = askQty,
-                       OA_pair_original = original_symbol_pair,
-                       OA_minQty_filter = minQty_filter),
+    left_join(order_best_all %>% select(symbol_pair,
+                                        OA_pair_askPrice = askPrice,
+                                        OA_pair_askQty   = askQty,
+                                        OA_minQty_filter = minQty_filter), 
               by = c("pair_OA" = "symbol_pair")) %>% 
-    mutate(OA_Value_in_O = OA_pair_askQty/OA_pair_askPrice) %>% 
-    
-    left_join(order_best_all %>% 
-                select(symbol_pair,
-                       AB_pair_askPrice = askPrice,
-                       AB_pair_askQty   = askQty,
-                       AB_pair_original = original_symbol_pair,
-                       AB_minQty_filter = minQty_filter),
-              by = c("pair_AB" = "symbol_pair")) %>%
     
     left_join(order_best_all %>% 
                 select(symbol_pair,
                        BO_pair_bidPrice = bidPrice,
                        BO_pair_bidQty   = bidQty,
-                       BO_pair_original = original_symbol_pair,
                        BO_minQty_filter = minQty_filter),
               by = c("pair_BO" = "symbol_pair")) %>% 
     
-    mutate(AB_Value_in_O = AB_pair_askQty*BO_pair_bidPrice,
+    mutate(OA_Value_in_O = OA_pair_askQty*OA_pair_askPrice,
+           AB_Value_in_O = AB_pair_askQty*BO_pair_bidPrice,
            BO_Value_in_O = BO_pair_bidQty*BO_pair_bidPrice,
-           AB_Side       = ifelse(AB_pair_original == pair_AB, "SELL","BUY"),
            
-           OA_buy_quantity = O_value*OA_pair_askPrice,
+           OA_buy_quantity = O_value/OA_pair_askPrice,
            OA_buy_quantity_r = round(OA_buy_quantity/OA_minQty_filter, 0)*OA_minQty_filter,
            
-           AB_trade_quatity = OA_buy_quantity_r*(1-trading_fee_rate)*AB_pair_askPrice,
+           AB_trade_quatity = OA_buy_quantity_r*(1-trading_fee_rate)/AB_pair_askPrice,
            AB_trade_quatity_r = floor(AB_trade_quatity/AB_minQty_filter)*AB_minQty_filter,
            
            BO_trade_quatity = AB_trade_quatity_r*(1-trading_fee_rate),
            BO_trade_quatity_r = floor(BO_trade_quatity/BO_minQty_filter)*BO_minQty_filter
     ) %>% 
-    mutate(result_O = BO_trade_quatity_r*BO_pair_bidPrice*(1-trading_fee_rate),
-           profit_O = result_O-OA_buy_quantity_r/OA_pair_askPrice,
-           profit_O_percentage = 100*profit_O/(OA_buy_quantity_r/OA_pair_askPrice)) %>% 
+    mutate(result_O = AB_trade_quatity*(1-trading_fee_rate)^2*BO_pair_bidPrice,
+           profit_O = result_O-OA_buy_quantity_r*OA_pair_askPrice,
+           profit_O_percentage = 100*profit_O/(OA_buy_quantity_r*OA_pair_askPrice))
+
+    data.table::fwrite(as.data.frame(decide_trade_df),  append = T,
+                       file = paste0("trade_results.csv"))
     
-    filter(!is.na(profit_O),
-           profit_O_percentage  > 0.002
-           ,
-           OA_Value_in_O > 20*O_value,
-           AB_Value_in_O > 20*O_value,
-           BO_Value_in_O > 20*O_value
-           ) %>% 
+    decide_trade_df %>% filter(profit_O_percentage  > 0.0015,
+           OA_Value_in_O > 10*O_value,
+           AB_Value_in_O > 10*O_value,
+           BO_Value_in_O > 10*O_value) %>% 
     mutate(time = Sys.time()) %>% 
     arrange(-profit_O) %>% 
     head(1)
@@ -152,44 +116,53 @@ decide_trade_or_no <- function(O_value){
 trade_main <- function(O_value = 15){
   
   price_qty_df <- decide_trade_or_no(O_value)
+
   # Buy order at market price
   if(nrow(price_qty_df) == 1 ){
+    price_qty_df$OA_pair_original
+    price_qty_df$OA_pair_askPrice
     
-    if(price_qty_df$AB_Side == "BUY"){
-      
-      # Buy order at market price
-      OA_buy_result <- place_order_limit(symbol   = price_qty_df$OA_pair_original,
-                                         side     = "BUY",
-                                         quantity = price_qty_df$OA_buy_quantity_r,
-                                         price    = 1/price_qty_df$OA_pair_askPrice,
-                                         timeInForce = "FOK")
-      
-      if(content(OA_buy_result)$status == "FILLED"){
-        
-        # Buy order at market price
-        AB_buy_result <- binance_post(endpoint = '/api/v3/order',
-                                      params = list(symbol   = price_qty_df$AB_pair_original,
-                                                    side     = price_qty_df$AB_Side,
-                                                    type     = "MARKET",
-                                                    quantity = as.character(price_qty_df$AB_trade_quatity_r)))
-        # content(AB_buy_result)
-        
-        # Sell order at market price
-        BO_sell_result <- binance_post(endpoint = '/api/v3/order',
-                                       params = list(symbol   = price_qty_df$BO_pair_original,
-                                                     side     = "SELL",
-                                                     type     = "MARKET",
-                                                     quantity = as.character(price_qty_df$BO_trade_quatity_r) ))
-        # content(BO_sell_result)
-        
-        print(paste("Traded at", Sys.time()))
-        data.table::fwrite(as.data.frame(price_qty_df),  append = T,
-                           file = paste0("trade_results.csv"))
-        browser()
-        
-      }
-
-    }
+    # Buy order at market price
+    # OA_buy_result <- place_order_limit(symbol   = price_qty_df$OA_pair_original,
+    #                                    side     = "BUY",
+    #                                    quantity = price_qty_df$OA_buy_quantity_r,
+    #                                    price    = 1/price_qty_df$OA_pair_askPrice,
+    #                                    timeInForce = "GTC")
+    OA_buy_result <- place_order_market(symbol   = price_qty_df$OA_pair_original,
+                                       side     = "BUY",
+                                       quantity = price_qty_df$OA_buy_quantity_r)
+    # if(content(OA_buy_result)$status == "FILLED"){
+    
+    # Buy order at market price
+    # AB_buy_result <- place_order_limit(symbol   = price_qty_df$AB_pair_original,
+    #                                    side     = price_qty_df$AB_Side,
+    #                                    quantity = price_qty_df$AB_trade_quatity_r,
+    #                                    price    = price_qty_df$AB_pair_askPrice,
+    #                                    timeInForce = "GTC")
+    AB_buy_result <- place_order_market(symbol   = price_qty_df$AB_pair_original,
+                                        side     = "BUY",
+                                        quantity = price_qty_df$AB_trade_quatity_r)
+    # content(AB_buy_result)
+    
+    # Sell order at market price
+    # BO_buy_result <- place_order_limit(symbol   = price_qty_df$OB_pair_original,
+    #                                    side     = "SELL",
+    #                                    quantity = price_qty_df$BO_trade_quatity_r,
+    #                                    price    = price_qty_df$BO_pair_bidPrice,
+    #                                    timeInForce = "GTC")
+    BO_buy_result <- place_order_limit(symbol   = price_qty_df$OB_pair_original,
+                                       side     = "SELL",
+                                       quantity = price_qty_df$BO_trade_quatity_r)
+    # content(BO_sell_result)
+    
+    print(paste("Traded at", Sys.time()))
+    # data.table::fwrite(as.data.frame(price_qty_df),  append = T,
+    #                    file = paste0("trade_results.csv"))
+    browser()
+    
+    # }
+    
+    
   }  else{print(paste("No trigger for Trading at", Sys.time()))}
   
 }
@@ -202,3 +175,4 @@ while(TRUE){
   p2 = proc.time() - p1
   Sys.sleep(max((3 - p2[3]), 0)) #basically sleep for whatever is left of the second
 }
+
